1efefafc4e88aef4f5b78209f2f6ab91f025067a
Hasan Al-Snaid <Hasan.Snaid@info-seed.com>
2024-09-05 19:00:40 +0300

    add Connection Strings

--- EmployeeManagementAPI/EmployeeManagementAPI/Program.cs
+++ EmployeeManagementAPI/EmployeeManagementAPI/Program.cs
@@ -1,4 +1,21 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy =>
+        {
+            policy.AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowAnyOrigin(); // Use .WithOrigins("http://localhost:4200") for more restrictive policies
+        });
+});
+
 
 // Add services to the container.
 
@@ -6,6 +23,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+// Register the EmployeeContext with the connection string
+builder.Services.AddDbContext<EmployeeManagementContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
@@ -15,6 +40,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseCors("AllowAllOrigins");
 
 app.UseHttpsRedirection();
 
